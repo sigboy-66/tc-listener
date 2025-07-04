963b6a55537a63ff20980f644f369e1b61f42d90
sigboy-66 <sigboy66@yahoo.com>
2025-07-04 16:52:13 -0600

    Initial check in

--- sender.py
+++ sender.py
@@ -10,12 +10,12 @@ if len(sys.argv) > 1:
     loop_count = int(sys.argv[1])
 
 
-server_ip = '127.0.0.1'  # Change to server's IP if needed
-server_port = 4446
+receiver_ip = '127.0.0.1'  # Change to server's IP if needed
+receiver_port = 4446
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-    client_socket.connect((server_ip, server_port))
-    print(f"Connected to server at {server_ip}:{server_port}")
+    client_socket.connect((receiver_ip, receiver_port))
+    print(f"Connected to server at {receiver_ip}:{receiver_port}")
 
     try:
         for heartbeat_num in range(loop_count):
