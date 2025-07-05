973e4fdd0c5b9de44de5296275bfda19f2c6d24c
sigboy-66 <sigboy66@yahoo.com>
2025-07-05 16:12:47 -0600

    Initial check in

---
+++ config.py
@@ -0,0 +1,3 @@
+heartbeat_interval = 3.0  # time interval to send heartbeats
+num_beats = 10          # number of beats to send in the test
+tcp_port = 4445           # tcp port to listen and send on
